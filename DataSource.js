9ba2616766e18440dfb95ec3b03913c46fcfea9b
egadams <31349790+EGAdams@users.noreply.github.com>
2022-08-22 12:39:56 -0400

    insert php wip

--- DataSource.js
+++ DataSource.js
@@ -2,17 +2,46 @@
 class DataSource {
     constructor(config) { this.url = config.getUrl(); } // establish communication address
 
-    async getObjects() {  // copilot wrote most of this.
+    async getObjects( resultProcessorArg ) {
+        this.resultProcessor = resultProcessorArg;
         fetch( this.url )
             .then((response) => {
-                response.text().then((text) => {
-                    console.log( "text: " + text );
+                response.text().then(( result ) => {
+                    console.log( "result: " + result );
+                    console.log( "processing text with [ " + this.resultProcessor + " ]" );
                 });
             }).catch((error) => {
-                console.log( "error: " + error.text );
+                console.log( "error: " + error.stack );
             }).then(() => {
                 console.log( "done" );
             });
+    }
+
+    async insertObject( object_view_id_arg, object_data_arg ) {
+        console.log( "insertObject: " + object_view_id_arg + " " + object_data_arg );
+        fetch( this.url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                object_view_id: object_view_id_arg,
+                object_data: object_data_arg
+            })
+        }).then((response) => {
+            response.text().then(( result ) => {
+                console.log( "result: " + result );
+            }).catch((error) => {
+                console.log( "error: " + error.stack );
+            }).then(() => {
+                console.log( "done" );
+            }).catch((error) => {
+                console.log( "error: " + error.stack );
+            });
+        }).catch((error) => {
+            console.log( "error: " + error.stack );
+        });
     }
 }
 
--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 import DataSource from './DataSource.js';
 import MonitoredObjectConfig from './MonitoredObjectConfig.js';
 
-const config = { "url": "https://americansjewelry.com/test2/runQ.php/index.php/user/list?limit=20" };
-// const config = { "url": "http://localhost:60191/index.php/user/list?limit=20" };
+const port = "51492";
+// const config = { "url": "https://americansjewelry.com/test2/runQ.php/index.php/user/list?limit=20" };
+const config = { "url": "http://localhost:" + port + "/index.php/user/insert" };
 const source = new DataSource( new MonitoredObjectConfig( config ));
-let theResponse = await source.getObjects();
+
+let theResponse = await source.insertObject( "TestObject_1999", JSON.stringify( source ));
 console.log( "theResponse: " +  theResponse );
