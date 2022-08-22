742be198380aae8ecc52f28bd2d729e52c3e7667
egadams <31349790+EGAdams@users.noreply.github.com>
2022-08-22 18:48:11 -0400

    got data selecting all

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 import DataSource from './DataSource.js';
 import MonitoredObjectConfig from './MonitoredObjectConfig.js';
 
-const port = "51905";
-// const config = { "url": "https://americansjewelry.com/test2/runQ.php/index.php/user/list?limit=20" };
+const port = "53924";
+const config = { "url": "https://americansjewelry.com/test2/runQ.php/index.php/user/list" };
 // const config = { "url": "http://localhost:" + port + "/index.php/user/list?limit=20" };
-const config = { "url": "https://americansjewelry.com/test2/runQ.php/index.php/user/insert" };
-//const config = { "url": "http://localhost:" + port + "/index.php/user/insert" };
+// const config = { "url": "https://americansjewelry.com/test2/runQ.php/index.php/user/insert" };
+// const config = { "url": "http://localhost:" + port + "/index.php/user/insert" };
 
 const source = new DataSource( new MonitoredObjectConfig( config ));
-let theResponse = await source.insertObject( "TestObject_1999", JSON.stringify( source ));
-// let theResponse = await source.getObjects();
+// let theResponse = await source.insertObject( "TestObject_1999", JSON.stringify( source ));
+let theResponse = await source.getObjects();
 
 console.log( "theResponse: " +  theResponse );
