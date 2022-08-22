ffc7bb53c1f40b3955cdb4ca62dd52f46200c413
egadams <31349790+EGAdams@users.noreply.github.com>
2022-08-22 19:49:32 -0400

    insert object works.  cleaned up...

--- DataSource.js
+++ DataSource.js
@@ -5,43 +5,27 @@ class DataSource {
     async getObjects( resultProcessorArg ) {
         this.resultProcessor = resultProcessorArg;
         fetch( this.url )
-            .then((response) => {
-                response.json().then(( result ) => {
-                    console.log( "result: " + result );
-                    // console.log( "processing text with [ " + this.resultProcessor + " ]" );
-                });
-            }).catch((error) => {
-                console.log( "error: " + error.stack );
-            }).then(() => {
-                console.log( "done" );
-            });
-    }
+            .then((  response ) => { response.json()
+            .then((  result   ) => { this.resultProcessor( result ); });     })
+            .catch(( error    ) => { console.log( "error: " + error.stack ); })
+            .then(()            => { console.log( "done" );                  }); }
 
     async insertObject( object_view_id_arg, object_data_arg ) {
         console.log( "insertObject: " + object_view_id_arg + " " + object_data_arg );
         fetch( this.url, {
             method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                object_view_id: object_view_id_arg,
-                object_data: object_data_arg
-            })
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+            body: JSON.stringify({ object_view_id: object_view_id_arg, object_data: object_data_arg })
         }).then((response) => {
             response.json().then(( result ) => {
                 console.log( "result: " + result );
-                // console.log( "processing text with [ " + this.resultProcessor + " ]" );
             }).catch(( error ) => { 
                 console.log( "error: " + error.stack ); 
             });
         }).catch((error) => {
             console.log( "error: " + error.stack );
         }).then(() => {
-            console.log( "done" );
-        });
-    }
+            console.log( "done" ); }); }
 }
 
 export default DataSource;
--- test.js
+++ test.js
@@ -2,13 +2,15 @@ import DataSource from './DataSource.js';
 import MonitoredObjectConfig from './MonitoredObjectConfig.js';
 
 const port = "53924";
-const config = { "url": "https://americansjewelry.com/test2/runQ.php/index.php/user/list" };
+// const config = { "url": "https://americansjewelry.com/test2/runQ.php/index.php/user/list" };
 // const config = { "url": "http://localhost:" + port + "/index.php/user/list?limit=20" };
-// const config = { "url": "https://americansjewelry.com/test2/runQ.php/index.php/user/insert" };
+const config = { "url": "https://americansjewelry.com/test2/runQ.php/index.php/user/insert" };
 // const config = { "url": "http://localhost:" + port + "/index.php/user/insert" };
 
 const source = new DataSource( new MonitoredObjectConfig( config ));
-// let theResponse = await source.insertObject( "TestObject_1999", JSON.stringify( source ));
-let theResponse = await source.getObjects();
+function gotData( data ) { console.log( "got data: " + data ); }
+
+let theResponse = await source.insertObject( "AnotherTestObject_1999", JSON.stringify( source ));
+// let theResponse = await source.getObjects( gotData );
 
 console.log( "theResponse: " +  theResponse );
